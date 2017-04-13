d3c46ce2a7bbd901de723c4f8e9e92209643002f
Pudge Fan <fannailiang2009@gmail.com>
2017-04-13 11:28:12 +0800

    添加JobController。

---
+++ src/main/java/com/aerors/dms/controller/JobController.java
@@ -0,0 +1,10 @@
+package com.aerors.dms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+}
