9f0d632c42cc2c2557842f05613ab99d096824f7
Pudge Fan <fannailiang2009@gmail.com>
2017-04-25 15:07:04 +0800

    为系统配置实体类添加修改标识位。

--- src/main/java/com/aerors/dms/model/SystemConfigContentModel.java
+++ src/main/java/com/aerors/dms/model/SystemConfigContentModel.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.Id;
 
 /** 
 * @ClassName: SystemConfigContentModel 
-* @Description: ArchiveTaskJob(这里用一句话描述这个类的作用)
+* @Description: 该类为系统参数实体类
 *  
 * @author pudge.fan(pudge_fan@163.com) 
 * @date 2017年4月25日 下午2:19:32 
@@ -29,6 +29,9 @@ public class SystemConfigContentModel implements Serializable{
 	/** 系统参数Value **/
 	private String systemConfigContentValue;
 	
+	/** 是否可修改标识：0：不可修改；1:可以修改 **/
+	private String modifyFlag;
+	
 	public String getSystemConfigContentId() {
 		return systemConfigContentId;
 	}
@@ -46,5 +49,11 @@ public class SystemConfigContentModel implements Serializable{
 	}
 	public void setSystemConfigContentValue(String systemConfigContentValue) {
 		this.systemConfigContentValue = systemConfigContentValue;
+	}
+	public String getModifyFlag() {
+		return modifyFlag;
+	}
+	public void setModifyFlag(String modifyFlag) {
+		this.modifyFlag = modifyFlag;
 	}
 }
