0de12f93e0804596c899a05ea56fe7c0f8f80e00
Pudge Fan <fannailiang2009@gmail.com>
2017-04-28 09:07:37 +0800

    添加系统参数Dao层的接口及实现类。

---
+++ src/main/java/com/aerors/dms/dao/impl/SystemConfigContentDaoImpl.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright © 2017 西安航天天绘数据技术有限公司制图与地理信息室所有
+ */
+package com.aerors.dms.dao.impl;
+
+import com.aerors.dms.dao.ISystemConfigContentDao;
+import com.aerors.dms.model.SystemConfigContentModel;
+
+/** 
+* @ClassName: SystemConfigContentDaoImpl 
+* @Description: systemConfigContentModel的Dao层实现类
+*  
+* @author pudge.fan(pudge_fan@163.com) 
+* @date 2017年4月28日 上午8:58:01 
+* 
+*/
+public class SystemConfigContentDaoImpl extends ABaseMongoDao<SystemConfigContentModel> implements ISystemConfigContentDao {
+
+	@Override
+	protected Class<SystemConfigContentModel> getEntityClass() {
+		return SystemConfigContentModel.class;
+	}
+
+}
